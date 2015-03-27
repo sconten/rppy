22495f0195529e58f58c9f13743afa6d4d6204cf
shear <sean.contenti@gmail.com>
2015-03-26 23:06:18 -0600

    test_rppy

--- tests/test_rppy.py
+++ tests/test_rppy.py
@@ -9,21 +9,7 @@ Tests for `rppy` module.
 """
 
 import unittest
-import numpy
 from rppy import rppy
-
-
-def test_voight_bound():
-    K = numpy.array([36, 2.2])
-    f = numpy.array([0.6, 0.4])
-    
-    assert numpy.abs(rppy.voight_bound(K, f)) < 0.01
-    
-def test_reuss_bound():
-    K = numpy.array([36, 2.2])
-    f = numpy.array([0.6, 0.4])
-    
-    assert numpy.abs(rppy.reuss_bound(K, f) - 5.04) < 0.01
 
 class TestRppy(unittest.TestCase):
 
