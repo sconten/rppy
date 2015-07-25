2ece3d58ae1d485e894590bf49d574083931baa0
sconten <sean.contenti@gmail.com>
2015-07-25 16:12:20 -0600

    Working on ortho tests

--- temp_test_ortho.py
+++ temp_test_ortho.py
@@ -2,53 +2,47 @@ import rppy
 import numpy as np
 import matplotlib.pyplot as plt
 
-thetas = np.arange(1, 89, 1)
-Rphti = np.zeros(np.shape(thetas))
-Rp = np.zeros(np.shape(thetas))
-phi = 45
+vp1 = 3000
+vs1 = 1500
+p1 = 2000
+e1_1 = 0.0
+d1_1 = 0.0
+y1_1 = 0.0
+e2_1 = 0.0
+d2_1 = 0.0
+y2_1 = 0.0
+d3_1 = 0.0
+chi1 = 0.0
+C1 = rppy.reflectivity.Cij(vp1, vs1, p1, e1_1, d1_1, y1_1, e2_1, d2_1, y2_1, d3_1)
 
-vp1 = 2260
-vs1 = 1428
-p1 = 2600
-chi1 = 0
-C1 = np.array([[1.32797600e+10, 2.67601320e+09, 2.67600320e+09, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00],
-               [2.67600320e+09, 1.32797600e+10, 2.67600320e+09, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00],
-               [2.67600320e+09, 2.67600320e+09, 1.32797600e+10, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00],
-               [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 5.30187840e+09, 0.00000000e+00, 0.00000000e+00],
-               [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 5.30187840e+09, 0.00000000e+00],
-               [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 5.30187840e+09]])
-vp1, vs1, e11, d11, y11, e21, d21, y21, d31 = rppy.reflectivity.thomsen(C1, p1)
-Cbar = rppy.reflectivity.Cij(vp1, vs1, p1, e11, d11, y11, e21, d21, y21, d31)
+vp2 = 4000
+vs2 = 2000
+p2 = 2200
+e1_2 = 0.0
+d1_2 = 0.0
+y1_2 = 0.0
+e2_2 = 0.1
+d2_2 = 0.1
+y2_2 = 0.3
+d3_2 = 0.0
+chi2 = 0.0
+C2 = rppy.reflectivity.Cij(vp2, vs2, p2, e1_2, d1_2, y1_2, e2_2, d2_2, y2_2, d3_2)
 
-p2 = 2700
-chi2 = 0
-C2 = np.array([[1.46039400e+10, 4.98602000e+09, 4.98602000e+09, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00],
-               [4.98602000e+09, 1.46039400e+10, 4.98601000e+09, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00],
-               [4.98602000e+09, 4.98602000e+09, 1.46039400e+10, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00],
-               [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 4.80896000e+09, 0.00000000e+00, 0.00000000e+00],
-               [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 4.80896000e+09, 0.00000000e+00],
-               [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 4.80896000e+09]])
-vp2, vs2, e12, d12, y12, e22, d22, y22, d32 = rppy.reflectivity.thomsen(C2, p2)
+phi = np.arange(0, 90, 1)
+theta = np.arange(0, 90, 1)
 
-for n in thetas:
-    Rphti[n-1] = rppy.reflectivity.exact_ortho(C1, p1, C2, p2, chi1, chi2, phi, thetas[n-1])
+loopang = phi
+theta = np.array([30])
 
-Rp = rppy.reflectivity.zoeppritz(vp1, vs1, p1, vp2, vs2, p2, np.radians(thetas))
-Rp_rug = rppy.reflectivity.ruger_hti(vp1, vs1, p1, e21, d21, y21, vp2, vs2, p2, e22, d22, y22, np.radians(thetas), np.radians(phi))
+rphti = np.zeros(np.shape(loopang))
+rpzoe = np.zeros(np.shape(loopang))
+rprug = np.zeros(np.shape(loopang))
+
+for aid, val in enumerate(loopang):
+    rphti[aid] = rppy.reflectivity.exact_ortho(C1, p1, C2, p2, chi1, chi2, loopang[aid], theta)
+    rprug[aid] = rppy.reflectivity.ruger_hti(vp1, vs1, p1, e2_1, d2_1, y2_1, vp2, vs2, p2, e2_2, d2_2, y2_2, np.radians(theta), np.radians(loopang[aid]))
+    rpzoe[aid] = rppy.reflectivity.zoeppritz(vp1, vs1, p1, vp2, vs2, p2, np.radians(theta))
 
 plt.figure(1)
-plt.plot(thetas, Rphti, thetas, Rp, thetas, Rp_rug)
-plt.legend(['HTI', 'Zoe', 'Ruger'])
-plt.xlim([10, 70])
-plt.ylim([0, 0.2])
+plt.plot(loopang, rphti, loopang, rprug, loopang, rpzoe)
 plt.show()
-np.set_printoptions(precision=2)
-print()
-print('Original C')
-print(C1)
-print()
-print('Paras')
-print(vp1, vs1, p1, e11, d11, y11, e21, d21, y21, d31)
-print()
-print('New C')
-print(Cbar)
