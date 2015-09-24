8c4c1ed2edc2055b373e9b064037b4b8d856b994
sconten <sean.contenti@gmail.com>
2015-09-23 21:15:55 -0600

    Daley Hron 1977 update

--- rppy/reflectivity.py
+++ rppy/reflectivity.py
@@ -326,9 +326,7 @@ def ruger_vti(Vp1, Vs1, p1, e1, d1,
     return(Rpp)
 
 
-def daley_hron_vti(V1, V2, V3, V4, p1, p2, theta1,
-                   C1_11, C1_13, C1_33, C1_55,
-                   C2_11, C2_13, C2_33, C2_55):
+def daley_hron_vti(V1, V2, V3, V4, p1, p2, theta1, C1, C2, C3, C4):
     """
     Returns the exact reflectivity coefficients for a VTI medium computed using
     the relations of Daley and Hron (1977).
@@ -342,10 +340,54 @@ def daley_hron_vti(V1, V2, V3, V4, p1, p2, theta1,
     :param theta1: Incidence angle of incident P-wave in upper medium.
     """
 
-    # TODO: There's gotta be a better way to implement these equation than
-    #       transcribing them from the 1977 paper. This is insane.
     theta1 = np.radians(theta1)
     theta2, theta3, theta4, p = snell(V1, V2, V3, V4, theta1)
+
+    A1 = C1 / p1
+    A2 = C2 / p2
+    A3 = C3 / p1
+    A4 = C4 / p2
+
+    A1_1 = (2*(A1[0][2] + A1[4][4])**2 -
+            (A1[2][2] - A1[4][4])*(A1[0][0] + A1[2][2] - 2*A1[4][4]))
+    A1_2 = (2*(A2[0][2] + A2[4][4])**2 -
+            (A2[2][2] - A2[4][4])*(A2[0][0] + A2[2][2] - 2*A2[4][4]))
+    A1_3 = (2*(A3[0][2] + A3[4][4])**2 -
+            (A3[2][2] - A3[4][4])*(A3[0][0] + A3[2][2] - 2*A3[4][4]))
+    A1_4 = (2*(A4[0][2] + A4[4][4])**2 -
+            (A4[2][2] - A4[4][4])*(A4[0][0] + A4[2][2] - 2*A4[4][4]))
+
+    A2_1 = (A1[0][0] + A1[2][2] - 2*A1[4][4])**2 - 4*(A1[0][2] + A1[4][4])**2
+    A2_2 = (A2[0][0] + A2[2][2] - 2*A2[4][4])**2 - 4*(A2[0][2] + A2[4][4])**2
+    A2_3 = (A3[0][0] + A3[2][2] - 2*A3[4][4])**2 - 4*(A3[0][2] + A3[4][4])**2
+    A2_4 = (A4[0][0] + A4[2][2] - 2*A4[4][4])**2 - 4*(A4[0][2] + A4[4][4])**2
+
+    Q1 = np.sqrt((A1[2][2] - A1[4][4])**2 +
+                 2*A1_1*np.sin(theta1) + A2_1*np.sin(theta1))
+    Q2 = np.sqrt((A2[2][2] - A2[4][4])**2 +
+                 2*A1_2*np.sin(theta2) + A2_2*np.sin(theta2))
+    Q3 = np.sqrt((A3[2][2] - A3[4][4])**2 +
+                 2*A1_3*np.sin(theta3) + A2_3*np.sin(theta3))
+    Q4 = np.sqrt((A4[2][2] - A4[4][4])**2 +
+                 2*A1_4*np.sin(theta4) + A2_4*np.sin(theta4))
+
+    l1 = np.sqrt(((Q1 - A1[2][2] + A1[4][4])/np.sin(theta1)**2 +
+                 (A1[0][0] + A1[2][2] - 2*A1[4][4]))/(2*Q1))
+    l2 = np.sqrt(((Q2 - A2[2][2] + A2[4][4])/np.sin(theta2)**2 +
+                 (A2[0][0] + A2[2][2] - 2*A2[4][4]))/(2*Q2))
+    l3 = np.sqrt(((Q3 - A3[2][2] + A3[4][4])/np.sin(theta3)**2 +
+                 (A3[0][0] + A3[2][2] - 2*A3[4][4]))/(2*Q3))
+    l4 = np.sqrt(((Q4 - A4[2][2] + A4[4][4])/np.sin(theta4)**2 +
+                 (A4[0][0] + A4[2][2] - 2*A4[4][4]))/(2*Q4))
+
+    m1 = np.sqrt(((Q1 - A1[0][0] + A1[4][4])/np.cos(theta1)**2 +
+                 (A1[0][0] + A1[2][2] - 2*A1[4][4]))/(2*Q1))
+    m2 = np.sqrt(((Q2 - A2[0][0] + A2[4][4])/np.cos(theta2)**2 +
+                 (A2[0][0] + A2[2][2] - 2*A2[4][4]))/(2*Q2))
+    m3 = np.sqrt(((Q3 - A3[0][0] + A3[4][4])/np.cos(theta3)**2 +
+                 (A3[0][0] + A3[2][2] - 2*A3[4][4]))/(2*Q3))
+    m4 = np.sqrt(((Q4 - A4[0][0] + A4[4][4])/np.cos(theta4)**2 +
+                 (A4[0][0] + A4[2][2] - 2*A4[4][4]))/(2*Q4))
 
     x = np.sin(theta1)
     n = V1/V2
@@ -353,85 +395,46 @@ def daley_hron_vti(V1, V2, V3, V4, p1, p2, theta1,
     k2 = V4/V2
 
     P = np.sqrt(1 - x**2)
-    Q = np.sqrt(1 - k1**2*x**2)
-    S = np.sqrt(1 - x**2/n**2)
-    R = np.sqrt(1 - k2**2*x**2/n**2)
+    Q = np.sqrt(1 - (k1*x)**2)
+    S = np.sqrt(1 - (x/n)**2)
+    R = np.sqrt(1 - (k2*x/n)**2)
 
-    # Upper medium parameters
-    A1_11 = C1_11 / p1
-    A1_33 = C1_33 / p1
-    A1_55 = C1_55 / p1
-    A1_13 = C1_13 / p1
-
-    A1_1 = 2*(A1_13 + A1_55)**2 - (A1_33 - A1_55)*(A1_11 + A1_33 - 2*A1_55)
-    A1_2 = (A1_11 + A1_33 - 2*A1_55)**2 - 4*(A1_13 + A1_55)**2
-    Q1 = np.sqrt((A1_33 - A1_55)**2 +
-                 2*A1_1*np.sin(theta1) +
-                 A1_2*np.sin(theta1))
-    l1 = np.sqrt(((Q1 - A1_33 + A1_55)/np.sin(theta1)**2 +
-                 (A1_11 + A1_33 - 2*A1_55))/(2*Q1))
-    m1 = np.sqrt(((Q1 - A1_11 + A1_55)/np.cos(theta1)**2 +
-                 (A1_11 + A1_33 - 2*A1_55))/(2*Q1))
-    l3 = np.sqrt(((Q1 - A1_33 + A1_55)/np.sin(theta3)**2 +
-                 (A1_11 + A1_33 - 2*A1_55))/(2*Q1))
-    m3 = np.sqrt(((Q1 - A1_11 + A1_55)/np.cos(theta3)**2 +
-                 (A1_11 + A1_33 - 2*A1_55))/(2*Q1))
-
-    d1 = l3*C1_33 - m3*C1_13
-    e1 = l1*C1_13 + (m1*C1_33 - l1*C1_13)*np.cos(theta1)**2
-    w1 = V1/V3*(1/(l1+m1))*(m3*np.cos(theta3)**2 - l3*np.sin(theta3)**2)
-    B1 = C1_55
-
-    # Lower medium parameters
-    A2_11 = C2_11 / p2
-    A2_33 = C2_33 / p2
-    A2_55 = C2_55 / p2
-    A2_55 = C2_55 / p2
-    A2_13 = C2_13 / p2
-
-    A2_1 = 2*(A2_13 + A2_55)**2 - (A2_33 - A2_55)*(A2_11 + A2_33 - 2*A2_55)
-    A2_2 = (A2_11 + A2_33 - 2*A2_55)**2 - 4*(A2_13 + A2_55)**2
-    Q2 = np.sqrt((A2_33 - A2_55)**2 +
-                 2*A2_1*np.sin(theta2) +
-                 A2_2*np.sin(theta2))
-    l2 = np.sqrt(((Q2 - A2_33 + A2_55)/np.sin(theta2)**2 +
-                 (A2_11 + A2_33 - 2*A2_55))/(2*Q2))
-    m2 = np.sqrt(((Q2 - A2_11 + A2_55)/np.cos(theta2)**2 +
-                 (A2_11 + A2_33 - 2*A2_55))/(2*Q2))
-    l4 = np.sqrt(((Q2 - A2_33 + A2_55)/np.sin(theta4)**2 +
-                 (A2_11 + A2_33 - 2*A2_55))/(2*Q2))
-    m4 = np.sqrt(((Q2 - A2_11 + A2_55)/np.cos(theta4)**2 +
-                 (A2_11 + A2_33 - 2*A2_55))/(2*Q2))
-
-    d2 = l4*C2_33 - m4*C2_13
-    e2 = V1/V2*(l2*C2_13 + (m2*C2_33 - l2*C2_13)*np.cos(theta2)**2)
-    w2 = V1/V4*(1/(l1+m1))*(m4*np.cos(theta4)**2 - l4*np.sin(theta4)**2)
-    B2 = C2_55
+    B1 = C1[4][4]
+    B2 = C2[4][4]
+    d1 = l3*C1[2][2] - m3*C1[0][2]
+    d2 = l4*C2[2][2] - m4*C2[0][2]
+    w1 = (V1/V3)*(1/(l1 + m1))*(m3*np.cos(theta3)**2 - l3*np.sin(theta3)**2)
+    w2 = (V1/V4)*(1/(l1 + m1))*(m4*np.cos(theta4)**2 - l4*np.sin(theta4)**2)
+    e1 = l1*C1[0][2] + (m1*C1[2][2] - l1*C1[0][2])*(np.cos(theta1)**2)
+    e2 = (V1/V2)*(l2*C2[0][2] + (m2*C2[2][2] - l2*C2[0][2])*np.cos(theta2)**2)
 
     l = (l2+m2)/(l1+m1)
 
+    # Eq. B-2
     T1 = e2 - (e1*l2)/(n*l1)
     T2 = B2*w2*k1*l3/m1 - B1*(w1*k2*l4)/(n*m1)
-    T3 = B2*w2 + B1*(k2*x**2*l4)/(n*m1)
-    T4 = e2*m3/l1 + (d1*x**2*l2)/(n*l1)
-    T5 = B1*(w1 + k1*x**2*l3/m1)
-    T6 = e2*m4/l1 + (d2*x**2*l2)/(n*l1)
+    T3 = B2*w2 + B1*(k2*(x**2)*l4)/(n*m1)
+    T4 = e2*m3/l1 + (d1*(x**2)*l2)/(n*l1)
+    T5 = B1*(w1 + k1*(x**2)*l3/m1)
+    T6 = e2*m4/l1 + (d2*(x**2)*l2)/(n*l1)
     T7 = B2*l - B1*m2/m1
     T8 = d2*m3/l1 - d1*m4/l1
-    T9 = B2*(w2*m2/m1 + k2*x**2*l*l4/(n*m1))
-    T10 = e1*m3/l1 + d1*x**2
-    T11 = e1*m4/l1 + d2*x**2
-    T12 = B2*k1*x**2*l*l3/m1 + B1*w1*m2/m1
+    T9 = B2*(w2*m2/m1 + k2*(x**2)*l*l4/(n*m1))
+    T10 = e1*m3/l1 + d1*(x**2)
+    T11 = e1*m4/l1 + d2*(x**2)
+    T12 = B2*k1*(x**2)*l*l3/m1 + B1*w1*m2/m1
 
-    E1 = T1*T2*x**2
+    # Eq. B-1
+    E1 = T1*T2*(x**2)
     E2 = T3*T4*P*Q
     E3 = T5*T6*P*R
-    E4 = T7*T8*x**2*P*Q*R*S
+    E4 = T7*T8*(x**2)*P*Q*R*S
     E5 = T9*T10*Q*S
     E6 = T11*T12*R*S
+
     D = E1 + E2 + E3 + E4 + E5 + E6
 
-    Rpp = (-E1 + E2 + E3 + E4 - E5 - E6)/D
+    Rpp = (-E1 + E2 + E3 + E4 - E5 - E6)/D  # Eq. 17
 
     return(Rpp)
 
